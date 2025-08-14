16c5de1675b4813c272b5ef6e3a4cb72b1380307
terry.chung <terry.chung@proagrica.com>
2025-08-14 13:59:24 +1000

    INTERLOK-4477: Upgrade to jakarta/Update release version

--- src/main/java/com/adaptris/core/transform/flatfile/FlatfileTransformService.java
+++ src/main/java/com/adaptris/core/transform/flatfile/FlatfileTransformService.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 
